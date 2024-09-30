cd98ab84fc5d77863f90b9916de9c80f4b584313
niNovikov <gambitrus38@yandex.ru>
2024-09-30 17:33:49 +0300

    clean

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,8 @@ import { store } from '../redux/store';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <style jsx global>
-        {`
-          html {
-            font-family: Lato
-          }
-        `}
-      </style>
       <Component {...pageProps} />
     </Provider>
-)
-}
+)}
 
 export default MyApp
